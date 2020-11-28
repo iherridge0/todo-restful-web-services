6a3f61f453d62970513db6269bca73fc1f7673eb
Irwin Herridge <irwinherridge3@gmail.com>
2020-11-28 17:44:28 +0200

    Create todos service #1
    Delete a Todo of a User
    DELETE /users/{username}/todo/{todo_id}

--- src/main/java/za/co/iherridge0/rest/webservices/todorestfulwebservices/todo/TodoResource.java
+++ src/main/java/za/co/iherridge0/rest/webservices/todorestfulwebservices/todo/TodoResource.java
@@ -4,7 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,10 +22,10 @@ public class TodoResource {
 
 	@Autowired
 	private ToDoRepository todoService;
-	
+
 	/**
-	 * Retrieve all Todos for a User
-	 * GET /users/{user_name}/todos
+	 * Retrieve all Todos for a User GET /users/{user_name}/todos
+	 * 
 	 * @param username
 	 * @return List of Todos
 	 */
@@ -32,20 +34,25 @@ public class TodoResource {
 	public List<Todo> retrieveAllTodos(@PathVariable String username) {
 		return todoService.findAll();
 	}
-	
-	//Delete a Todo of a User
-	//DELETE /users/{username}/todo/{todo_id}
 
-	//Edit/Update a Todo
-	//PUT /users/{username}/todos/{todo_id}
+	// Delete a Todo of a User
+	// DELETE /users/{username}/todo/{todoid}
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+		todoService.deleteById(id);		
+		return ResponseEntity.notFound().build();
+	}
 
-	//Create a new Todo
-	//POST /users/{username}/todos/
+	// Edit/Update a Todo
+	// PUT /users/{username}/todos/{todoid}
+
+	// Create a new Todo
+	// POST /users/{username}/todos/
 	@PostMapping("/users/{username}/todos")
 	public Todo createTodo(@RequestBody Todo todo, @PathVariable String username) {
 		Todo savedTodo = todoService.save(new Todo(username, todo.getDescription(), new Date(), false));
 		return savedTodo;
-		
+
 	}
-	
+
 }
--- src/main/java/za/co/iherridge0/rest/webservices/todorestfulwebservices/todo/repository/ToDoRepository.java
+++ src/main/java/za/co/iherridge0/rest/webservices/todorestfulwebservices/todo/repository/ToDoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import za.co.iherridge0.rest.webservices.todorestfulwebservices.todo.entity.Todo;
 
-public interface ToDoRepository extends JpaRepository<Todo, Integer> {
+public interface ToDoRepository extends JpaRepository<Todo, Long> {
 	
 }
