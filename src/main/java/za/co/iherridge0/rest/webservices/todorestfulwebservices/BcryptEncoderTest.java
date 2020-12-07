c1b7a7b24682ac18f83335e336490441622baac3
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-07 16:56:40 +0200

    Created master and develop branches and deleted main branch... not conforming to development standards

--- src/main/java/za/co/iherridge0/rest/webservices/todorestfulwebservices/BcryptEncoderTest.java
+++ src/main/java/za/co/iherridge0/rest/webservices/todorestfulwebservices/BcryptEncoderTest.java
@@ -7,7 +7,7 @@ public class BcryptEncoderTest {
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		for (int i = 1; i < 10; i++) {
-			String encodedString = encoder.encode("password1234!");
+			String encodedString = encoder.encode("password@!23@#!");
 			System.out.println(encodedString);
 		}
 	}
